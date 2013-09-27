9308c8b9176dd69bd4977d279bcc44928f9b86e1
Sébastien Cuendet <seb@wizzy.ch>
2013-09-27 09:52:59 +0200

    cleaned viewManager

--- packages/ViewManager/viewmanager.js
+++ packages/ViewManager/viewmanager.js
@@ -1,5 +1,3 @@
-var SHAPES = [6, 20, 64];
-
 ViewManager = function(){
     this.views = {};
 }
@@ -39,28 +37,24 @@ ViewManager.prototype.addStandardDisplayOptions = function() {
 		$('#transparency-off').removeClass('btn-primary');
 		$('#transparency-on').addClass('btn-primary');
 		that.setTransparency(true);
-		// views.setChangedLayout(true);
 	});
 
 	$('#transparency-off').on('click', function () {
 		$('#transparency-on').removeClass('btn-primary');
 		$('#transparency-off').addClass('btn-primary');
 		that.setTransparency(false);
-		// views.setChangedLayout(true);
 	});
 
 	$('#axis-on').on('click', function () {
 		$('#axis-off').removeClass('btn-primary');
 		$('#axis-on').addClass('btn-primary');
 		that.setAxis(true);
-		// views.setChangedLayout(true);
 	});
 
 	$('#axis-off').on('click', function () {
 		$('#axis-on').removeClass('btn-primary');
 		$('#axis-off').addClass('btn-primary');
 		that.setAxis(false);
-		// views.setChangedLayout(true);
 	});
 }
 
@@ -89,23 +83,11 @@ ViewManager.prototype.findClickedView = function(click){
     }
 }
 
-// ViewManager.prototype.setChangedLayout = function(bool){
-//     for (var i in this.views){
-//         this.views[i].changedLayout = bool; 
-//     }
-// };
-
 ViewManager.prototype.setAxis = function(bool){
     for (var i in this.views){
         this.views[i].axis = bool; 
     }
 };
-
-// ViewManager.prototype.setIsNotJittering = function(bool){
-//     for (var i in this.views){
-//         this.views[i].isNotJittering = bool; 
-//     }
-// };
 
 ViewManager.prototype.setClick = function(click){
     for (var i in this.views){
@@ -118,71 +100,6 @@ ViewManager.prototype.render = function(markers){
         this.views[i].render(markers);
     }
 };
-
-// ViewManager.prototype.activity2Difficulty = function(difficulty){
-//     for (var i in this.views){
-//         this.views[i].difficulty = difficulty;
-//     }
-// }
-
-// ViewManager.prototype.updateActivity2Feedback = function(markers){
-//     for (var i in this.views){
-//         this.views[i].updateActivity2Feedback(markers);
-//         break;
-//     }
-// }
-// 
-// ViewManager.prototype.checkActivity2Solution = function(markers){
-//     for (var i in this.views){
-//         if (this.views[i] instanceof FrontView){
-//             var count = [];
-//             var rotationOK = true;
-//             var positionOK = true;
-//             var diffRotation = [];
-//             for (var j in markers){
-//                 var marker = markers[j];
-//                 if (count.indexOf(marker.id) == -1){
-//                     count.push(marker.id);
-//                 }
-//                 for (var j = 0; j<ACTIVITYSHAPES.length; j++){
-//                     if (marker.id == ACTIVITYSHAPES[j]){
-//                         var Z = this.views[i].markerZ[marker.id];
-//                         var marker_position = [marker.corners[0].x, marker.corners[0].y,1];
-//                         var marker_position2 = [marker.corners[1].x, marker.corners[1].y,1];
-//                         position = pixel2mm(marker_position[0], marker_position[1], Z);
-//                         position2 = pixel2mm(marker_position2[0], marker_position2[1], Z);
-//                         var rotation;
-//                         var diffx = position.x-position2.x;
-//                         var diffy = position.y-position2.y;
-//                         if (diffy == 0){
-//                             diffy = 1;
-//                         }
-//                         if ((diffx < 0 && diffy > 0) || (diffx > 0 && diffy > 0)){
-//                             rotation = -Math.atan(diffx/diffy)+Math.PI/2;
-//                         }
-//                         else{
-//                             rotation = -Math.atan(diffx/diffy)-Math.PI/2;
-//                         }
-//                         diffRotation.push(rotation, ACTIVITYROTATION[marker.id],rotation - ACTIVITYROTATION[marker.id])
-//                         if (Math.abs(rotation - ACTIVITYROTATION[marker.id]) > 0.2){
-//                             rotationOK = false;
-//                         }
-//                         if (Math.abs(position.x - ACTIVITYTRANSLATION[marker.id][0]) > 20 || Math.abs(position.y - ACTIVITYTRANSLATION[marker.id][1]) > 20){
-//                             positionOK = false;
-//                         }
-//                     }
-//                 }
-//             }
-//             if (count.length == this.views[i].difficulty && rotationOK && positionOK){
-//                 var stopTime = new Date().getTime();
-//                 return stopTime;
-//             }
-//             else{
-//                 return null;
-//             }
-//         }
-//     }
-// }
 
 ViewManager.prototype.init_objects = function(){
 
@@ -198,7 +115,6 @@ ViewManager.prototype.clearChoiceEdges = function(){
     for(var i in this.views){
         this.views[i].edgesToSelect = [];
         this.views[i].levels = {};
-        // this.views[i].changedLayout = true;
     }
 }
 
@@ -219,10 +135,8 @@ ViewManager.prototype.showHelpOnSelect = function(click){
                 
             if (click.target.parentElement.id == currentView.name){
                 currentView.levels = {};
-                // currentView.changedLayout = true;
                 sideView.levels = {};
                 sideView.edgesToSelect = [];
-                // sideView.changedLayout = true;
                 helpingView.levels = {};
                 helpingView.edgesToSelect = [];
             }
@@ -230,8 +144,6 @@ ViewManager.prototype.showHelpOnSelect = function(click){
             for (var j in currentView.edgesToSelect){
                 helpingView.levels.y[j] = currentView.edgesToSelect[j];
             }
-            // helpingView.changedLayout = true;
-
         }
         else if (currentView instanceof SideView){
             var helpingView, frontView;
@@ -246,10 +158,8 @@ ViewManager.prototype.showHelpOnSelect = function(click){
                 
             if (click.target.parentElement.id == currentView.name){
                 currentView.levels = {};
-                // currentView.changedLayout = true;
                 frontView.levels = {};
                 frontView.edgesToSelect = [];
-                // frontView.changedLayout = true;
                 helpingView.levels = {};
                 helpingView.edgesToSelect = [];
             }
@@ -257,9 +167,6 @@ ViewManager.prototype.showHelpOnSelect = function(click){
             for (var j in currentView.edgesToSelect){
                 helpingView.levels.x[j] = currentView.edgesToSelect[j];
             }
-            
-            // helpingView.changedLayout = true;
-
         }
         else if (currentView instanceof TopView){
             var helpingView;
@@ -283,95 +190,6 @@ ViewManager.prototype.showHelpOnSelect = function(click){
             }
         }
     }
-}
-
-
-//ViewManager.prototype.generateRandomPositions = function(numberOfShapes, shapesToChooseFrom)
-//TODO put while in here, and return an array of shapes/objects
-
-	
-ViewManager.prototype.generateRandomPositions = function(){
-    var testObjects = [];
-    ACTIVITYSHAPES = [];
-    var count = 0;
-    var redoRandom = false;
-    var randomShape = Math.ceil(Math.random()*SHAPES.length)-1;
-    for (var name in this.views){
-            var view = this.views[name];
-            view.clear();
-            for (var i = 0; i<SHAPES.length; i++){
-                if (view.difficulty == 1){
-                    if (i == randomShape){
-                        var thisShape = SHAPES[i];
-                        var filledShape = view.shape(MODELS[thisShape]);
-                        testObjects[thisShape] = new THREE.Mesh(filledShape, new THREE.MeshBasicMaterial());
-                        ACTIVITYSHAPES.push(thisShape);
-                        ACTIVITYROTATION[thisShape] = Math.random()*Math.PI;
-                        ACTIVITYTRANSLATION[thisShape] = [Math.random()*WS_WIDTH-(WS_WIDTH/2),Math.random()*WS_HEIGHT-(WS_HEIGHT/2)];
-                    } 
-                }
-                else if (view.difficulty == 2){
-                    if (i != randomShape){
-                        var thisShape = SHAPES[i];
-                        var filledShape = view.shape(MODELS[thisShape]);
-                        testObjects[thisShape] = new THREE.Mesh(filledShape, new THREE.MeshBasicMaterial());
-                        ACTIVITYSHAPES.push(thisShape);
-                        ACTIVITYROTATION[thisShape] = Math.random()*Math.PI;
-                        ACTIVITYTRANSLATION[thisShape] = [Math.random()*WS_WIDTH-(WS_WIDTH/2),Math.random()*WS_HEIGHT-(WS_HEIGHT/2)];
-                    } 
-                }
-                else{
-                    var thisShape = SHAPES[i];
-                    var filledShape = view.shape(MODELS[thisShape]);
-                    testObjects[thisShape] = new THREE.Mesh(filledShape, new THREE.MeshBasicMaterial());
-                    ACTIVITYSHAPES.push(thisShape);
-                    ACTIVITYROTATION[thisShape] = Math.random()*Math.PI;
-                    ACTIVITYTRANSLATION[thisShape] = [Math.random()*WS_WIDTH-(WS_WIDTH/2),Math.random()*WS_HEIGHT-(WS_HEIGHT/2)];
-                }
-            }
-            for (var i in testObjects){
-                var object = testObjects[i];
-                object.rotation.z = ACTIVITYROTATION[i];
-                object.position.x = ACTIVITYTRANSLATION[i][0];
-                object.position.y = ACTIVITYTRANSLATION[i][1];
-                object.updateMatrix();
-                object.geometry.applyMatrix(object.matrix);
-                object.geometry.computeBoundingBox();
-            }
-            for (var i in testObjects){
-                var object1 = testObjects[i];
-                for (var i = 0; i < object1.geometry.vertices.length; i++){
-                    var vertex = object1.geometry.vertices[i];
-                    if (Math.abs(vertex.x) > (WS_WIDTH/2-40) || Math.abs(vertex.y) > (WS_HEIGHT/2-40)){
-                        redoRandom = true;
-                        break;
-                    }
-                }
-                if (redoRandom){
-                    break;
-                }
-            }
-            if (!redoRandom){
-                for (var i in testObjects){
-                    var object1 = testObjects[i];
-                    for (var j in testObjects){
-                        if (j != i){
-                            var object2 = testObjects[j];
-                            if (object1.geometry.boundingBox.isIntersectionBox(object2.geometry.boundingBox)){
-                                redoRandom = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (redoRandom){
-                        break;
-                    }
-                }
-            }
-        break;
-    }
-    testObjects = [];
-    return [redoRandom, ACTIVITYSHAPES];
 }
 
 ViewManager.prototype.destroy = function(){
