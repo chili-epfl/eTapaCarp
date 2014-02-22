77a161e3aa166a41a631e97834520af5b7b72c71
lorenzo <lorenzo@lorenzo-Vostro-3500.(none)>
2014-02-22 21:32:23 +0100

    new routing rules

--- client/eTapaCarp.js
+++ client/eTapaCarp.js
@@ -14,6 +14,11 @@ Session.setDefault('lang_id', 'fr');
 
 Session.setDefault('lang', {});
 
+Session.setDefault('virtual', false);
+
+/*Deps.autorun runs the function when 
+ *lang_id changes. It update the session collection
+ *lang. */
 Deps.autorun(function() {
   var data, langData, lang_id;
   langData = {};
@@ -31,33 +36,52 @@ setNav = function(name) {
   return Session.set('navbar:navId', name);
 };
 
+/*These functions takes an URL(the colon identifies a variable) and 
+ * return the templetes to render.
+ * https://github.com/tmeasday/meteor-router */
 Meteor.Router.add({
-  '/activity:id/practice': function(id){
+    '/activity:id/practice': function(id) {
+        if (Meteor.user()) {
     Session.set('isPractice', true);
-    return 'activity'+id;
-  },
+            if (id == 1) {
+                return 'activity' + id;
+            }
+            else if (id == 4 || id == 5) {
+                return 'activity'+id+"Difficulty";
+            }
+            else
+                return 'practiceActivity' + id;
+        }
+        else {
+            return 'notLoggedIn';
+        }
+    }, '/activity:id/practice/ready': function(id) {
+        if (Meteor.user()) {
+            Session.set('isPractice', true);
+            if (id == 1||id == 4 || id == 5) {
+                return 'activity' + id;
+            }
 
-  '/activity:id/scoring/difficulty': function(id){
-    if(Meteor.user()){
-      return 'activity'+id+'Difficulty';
+            else
+                return 'practiceActivity' + id;
     }
     else{
       return 'notLoggedIn';
     }
+    }
+    ,
+    '/activity:id/scoring/difficulty': function(id) {
+        if (Meteor.user()) {
+            return 'activity' + id + 'Difficulty';
+        }
+        else {
+            return 'notLoggedIn';
+        }
   },
-
-  '/activity:id/practice/difficulty': function(id){
-  	return 'activity'+id+'Difficulty';
-  },
-  
-  '/activity:id/practice/ready': function(id){
-	  return "activity"+id+"Ready";
-  },
-
-  '/activity:id/scoring/ready': function(id){
-    if(Meteor.Router.page() == 'activity'+id+'Difficulty' || Meteor.Router.page() == 'activity'+id+'Ready'){
-      if(Meteor.user()){
-        return 'activity'+id+'Ready';
+    '/activity:id/scoring/ready': function(id) {
+        if (Meteor.Router.page() == 'activity' + id + 'Difficulty' || Meteor.Router.page() == 'activity' + id + 'Ready') {
+            if (Meteor.user()) {
+                return 'activity' + id + 'Ready';
       }
       else{
         return 'notLoggedIn';
@@ -67,10 +91,9 @@ Meteor.Router.add({
       return 'notAllowed';
     }
   },
-
-  '/activity:id/scoring': function(id){
-    if(Meteor.Router.page() == 'activity'+id || Meteor.Router.page() == 'activity'+id+'Ready'){
-      if(Meteor.user()){
+    '/activity:id/scoring': function(id) {
+        if (Meteor.Router.page() == 'activity' + id || Meteor.Router.page() == 'activity' + id + 'Ready') {
+            if (Meteor.user()) {
         Session.set('isPractice', false);
         return 'activity'+id;
       }
@@ -80,6 +103,21 @@ Meteor.Router.add({
     }
     else{
       return 'notAllowed';
+    }
+  },
+  '/activity:id/practice/difficulty': function(id){
+  	return 'activity'+id+'Difficulty';
+  },
+  
+  '/activity:id/practice/ready': function(id){
+	  return "activity"+id+"Ready";
+  },
+  '/activity:id/scoring/difficulty': function(id){
+    if(Meteor.user()){
+      return 'activity'+id+'Difficulty';
+    }
+    else{
+      return 'notLoggedIn';
     }
   },
 
